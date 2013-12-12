219a015e281dbeeba4a613716455cd6fd95524f8
yannick <yannick@192.168.0.120>
2013-12-12 22:07:00 +0100

    AngularJS integration

--- angularjs-portal/app/js/controllers.js
+++ angularjs-portal/app/js/controllers.js
@@ -3,8 +3,14 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('MyCtrl1', ['$scope', function($scope) {
+    /**
+     * load swap
+     */
+    $scope.test = function() {
+        console.info('Book: ' + '/main/book/' + swap.id);
+        console.info();
+    }
   }])
   .controller('MyCtrl2', [function() {
 
--- angularjs-portal/app/js/services.js
+++ angularjs-portal/app/js/services.js
@@ -5,5 +5,47 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', ['ngResource'],
+        function($provide) {
+            /**
+             * swap provider
+             */
+            console.info('Register swap services');
+            $provide.factory('Swap', function($resource, $routeParams) {
+                /**
+                 * params injection
+                 * @type @exp;$routeParams@pro;swapId
+                 */
+                var swapId = $routeParams.swapId;
+                return $resource('', {}, {
+                    swaps: {
+                        url: '/joomla/3.2.0',
+                        params: {option:'com_swap', service:'Swap', method:'swaps', format:'json'}, 
+                        method: 'POST',
+                        isArray: true,
+                        cache: false
+                    },
+                    swap: {
+                        url: '/joomla/3.2.0', 
+                        params: {option:'com_swap', service:'Swap', method:'swap', format:'json', swapId: swapId}, 
+                        method: 'POST', 
+                        isArray: false, 
+                        cache: false
+                    },
+                    setup: {
+                        url: '/joomla/3.2.0', 
+                        params: {option:'com_swap', service:'Swap', method:'setup', format:'json'}, 
+                        method: 'POST', 
+                        isArray: false, 
+                        cache: false
+                    },
+                    planning: {
+                        url: '/joomla/3.2.0', 
+                        params: {option:'com_swap', service:'Swap', method:'planning', format:'json'}, 
+                        method: 'POST', 
+                        isArray: false, 
+                        cache: false
+                    }
+                });
+            });
+        }).value('version', '0.1');
